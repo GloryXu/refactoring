27d592ed2fcde1d723100b7e957685fe4a39ceaa
xuguangrong <xuguangrong@jd.com>
2018-08-09 21:55:41 +0800

    去除临时变量

---
+++ src/main/java/chapter01/step/five/Movie.java
@@ -0,0 +1,20 @@
+package chapter01.step.five;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Movie {
+
+    public static final int REGULAR = 0;
+    public static final int NEW_RELEASE = 1;
+    public static final int CHILDRENS = 2;
+
+    int priceCode;
+
+    String title;
+
+}
---
+++ src/main/java/chapter01/step/five/Rental.java
@@ -0,0 +1,50 @@
+package chapter01.step.five;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ *
+ */
+@Getter
+@Setter
+@ToString
+public class Rental {
+
+    Movie movie;
+
+    int daysRented;
+
+
+    public double getCharge() {
+        int result = 0;
+        // determine amount for each line
+        switch(getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if(getDaysRented() > 2)
+                    result += (getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += getDaysRented() *3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if(getDaysRented() > 3) {
+                    result += (getDaysRented() - 3) * 1.5;
+                }
+                break;
+        }
+        return result;
+    }
+
+    public int getFrequentRenterPoints() {
+        // add bonus for a two day new release rental
+        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
+            return 2;
+        else
+            return 1;
+    }
+}
