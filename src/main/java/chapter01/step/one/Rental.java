64169721096205aebd39064e880ca1e52a97074a
xuguangrong <xuguangrong@jd.com>
2018-08-09 21:22:52 +0800

    代码重构章节1的第一个例子

---
+++ src/main/java/chapter01/step/one/Rental.java
@@ -0,0 +1,20 @@
+package chapter01.step.one;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ *
+ */
+@Getter
+@Setter
+@ToString
+public class Rental {
+
+    Movie movie;
+
+    int daysRented;
+
+}
